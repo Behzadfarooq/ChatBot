0077095aabbc27d3db850d259f0d5a155927891d
Behzad Farooq <behzadsbz@gmail.com>
2023-11-18 12:30:53 +0530

    done

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,9 +1,26 @@
 import User from "../models/User.js";
-export const getAllUsers = async () => {
+import { hash } from "bcrypt";
+export const getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find();
+        return res.status(200).json({ message: "ok", users });
     }
     catch (error) {
+        console.log(error);
+        res.status(401).json({ message: "error", cause: error.message });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        return res.status(200).json({ message: "ok", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(401).json({ message: "error", cause: error.message });
     }
 };
 //# sourceMappingURL=user-controllers.js.map
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,10 +1,28 @@
+import { NextFunction, Request, Response, response } from "express";
 import User from "../models/User.js";
-
-export const getAllUsers = async() =>{
+import {hash} from "bcrypt"
+export const getAllUsers = async(req: Request , res: Response, next: NextFunction) =>{
     try {
         const users = await User.find()
-    
-    } catch (error) {
         
+         return res.status(200).json({message:"ok", users});
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({message:"error",cause : error.message});
+    }
+}
+
+
+
+export const userSignup = async(req: Request , res: Response, next: NextFunction) =>{
+    try {
+        const {name , email, password} = req.body;
+        const hashedPassword = await hash(password, 10)
+        const user = new User({name, email,password : hashedPassword})
+        await user.save()
+         return res.status(200).json({message:"ok", id:user._id.toString()});
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({message:"error",cause : error.message});
     }
 }
